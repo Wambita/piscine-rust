4499d941a569b15680c2e861fc3e32be2b10bc02
Wambita <wambitafana@gmail.com>
2025-04-03 22:18:51 +0300

    feat: temp conv

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * (5.0 / 9.0)
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * (9.0 / 5.0)) + 32.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fahrenheit_to_celsius() {
+        assert!((fahrenheit_to_celsius(-459.67) - (-273.15)).abs() < 1e-6);
+        assert!((fahrenheit_to_celsius(32.0) - 0.0).abs() < 1e-6);
+        assert!((fahrenheit_to_celsius(212.0) - 100.0).abs() < 1e-6);
+    }
+
+    #[test]
+    fn test_celsius_to_fahrenheit() {
+        assert!((celsius_to_fahrenheit(0.0) - 32.0).abs() < 1e-6);
+        assert!((celsius_to_fahrenheit(100.0) - 212.0).abs() < 1e-6);
+        assert!((celsius_to_fahrenheit(-40.0) - (-40.0)).abs() < 1e-6);
+    }
+}
