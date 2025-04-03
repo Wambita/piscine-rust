31d929e3bcf976245835bf94c753711b042303d5
Wambita <wambitafana@gmail.com>
2025-04-03 22:26:00 +0300

    feat: temp conv

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -12,15 +12,16 @@ mod tests {
 
     #[test]
     fn test_fahrenheit_to_celsius() {
-        assert!((fahrenheit_to_celsius(-459.67) - (-273.15)).abs() < 1e-6);
-        assert!((fahrenheit_to_celsius(32.0) - 0.0).abs() < 1e-6);
-        assert!((fahrenheit_to_celsius(212.0) - 100.0).abs() < 1e-6);
+        assert!(eql(fahrenheit_to_celsius(-459.67) - (-273.15)));
+        assert!(eql(fahrenheit_to_celsius(32.0) - 0.0));
+        assert!(eql(fahrenheit_to_celsius(212.0) - 100.0));
+        assert!(eql(fahrenheit_to_celsius(20.0), -6.666666666666666))
     }
 
     #[test]
     fn test_celsius_to_fahrenheit() {
-        assert!((celsius_to_fahrenheit(0.0) - 32.0).abs() < 1e-6);
-        assert!((celsius_to_fahrenheit(100.0) - 212.0).abs() < 1e-6);
-        assert!((celsius_to_fahrenheit(-40.0) - (-40.0)).abs() < 1e-6);
+        assert!(eql(celsius_to_fahrenheit(0.0) - 32.0));
+        assert!(eql(celsius_to_fahrenheit(100.0) - 212.0));
+        assert!(eql(celsius_to_fahrenheit(-40.0) - (-40.0)));
     }
 }
