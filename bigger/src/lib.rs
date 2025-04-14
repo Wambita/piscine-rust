38b5ed0a49aa6328817eed1d35f3fecdcbffb3f9
shfana <wambitafana@gmail.com>
2025-04-14 14:20:34 +0300

    rust

---
+++ bigger/src/lib.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut num: i32 = 0;
+    for (_, v) in h {
+        if v > num {
+            num = v
+        }
+    }
+    num
+}
