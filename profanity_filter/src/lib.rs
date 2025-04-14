45ec7857b8a413b934e5f3ec4228072df7c306cf
shfana <wambitafana@gmail.com>
2025-04-14 14:25:30 +0300

    feat: errors

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,27 @@
+pub struct Message {
+    content: String, 
+    user: String,
+}
+
+impl Message {
+    pub fn new(content: String, user: String) -> Self {
+        Message { content, user }
+    }
+
+    pub fn send_ms(&self) -> Option<String> {
+        if self.content.is_empty() || self.content.to_lowercase().contains("stupid") {
+            None
+        } else {
+            Some(self.content.clone())
+        }
+    }
+}
+
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    let msg = Message::new(message.to_string(), "user".to_string());
+    
+    match msg.send_ms() {
+        Some(_) => Ok(message),
+        None => Err("ERROR: illegal")
+    }
+}
