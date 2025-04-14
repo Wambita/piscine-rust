359e0da47389f423df58623f304b4f9a0ac57b66
shfana <wambitafana@gmail.com>
2025-04-14 13:53:19 +0300

    Feat: circle fix

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,41 +1,34 @@
-use std::f64::consts::PI;
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
+pub struct Point(pub f64, pub f64);
+#[derive(Debug, Clone, Copy)]
 pub struct Circle {
     pub center: Point,
     pub radius: f64,
 }
 
+impl Point {
+    pub fn distance(self, other: Point) -> f64 {
+        let x2 = (self.0 -other.0) * (self.0 - other.0);
+        let y2 = (self.1 -other.1) * (self.1 - other.1);
+        (x2+y2).sqrt()
+    }
+}
+
 impl Circle {
-    pub fn new(x: f64, y: f64, r: f64) -> Self {
+    pub fn new(x: f64, y: f64, radius: f64) -> Self{
         Circle {
-            center: Point { x: x, y: y },
-            radius: r,
+            center: Point(x,y),
+            radius,
         }
     }
-
-    pub fn diameter(&self) -> f64 {
-        2.0 * self.radius
+    pub fn diameter(self) -> f64 {
+        self.radius * 2.0
     }
-    pub fn area(&self) -> f64 {
-        PI * self.radius.powi(2)
+    pub fn area(self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
     }
-    pub fn intersect(&self, circle: &Circle) -> bool {
-        if self.center.distance(&circle.center) > circle.radius + self.radius {
-			return false;
-		}
-		true
-    }
-}
-
-#[derive(Debug)]
-pub struct Point {
-    pub x: f64,
-    pub y: f64,
-}
-
-impl Point {
-    pub fn distance(&self, point: &Point) -> f64 {
-        ((self.x - point.x).powi(2) + (self.y - point.y).powi(2)).sqrt()
+    pub fn intersect(self, other:Circle) ->  bool{
+        let distance = self.center.distance(other.center);
+        distance <= self.radius + other.radius
     }
 }
