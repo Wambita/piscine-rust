b9d595c9a42c8fdeb5bde1651c05fb1ab3a2f181
shfana <wambitafana@gmail.com>
2025-04-15 16:52:31 +0300

    feat: diamond creation

--- does_it_fit/src/areas_volumes.rs
+++ does_it_fit/src/areas_volumes.rs
@@ -1,89 +1,50 @@
-pub mod areas_volumes {
-    pub enum GeometricalShapes {
-        Square,
-        Circle,
-        Rectangle,
-        Triangle,
-    }
-
-    pub enum GeometricalVolumes {
-        Cube,
-        Sphere,
-        Cone,
-        TriangularPyramid,
-        Parallelepiped,
-    }
-
-    pub(crate) fn square_area(side: usize) -> usize {
-        side.pow(2)
-    }
-
-    pub(crate) fn triangle_area(base: usize, height: usize) -> f64 {
-        (base as f64 * height as f64) / 2.0
-    }
-
-    pub(crate) fn circle_area(radius: usize) -> f64 {
-        std::f64::consts::PI * (radius.pow(2) as f64)
-    }
-
-    pub(crate) fn rectangle_area(side_a: usize, side_b: usize) -> usize {
-        side_a * side_b
-    }
-
-    pub(crate) fn cube_volume(side: usize) -> usize {
-        side.pow(3)
-    }
-
-    pub(crate) fn sphere_volume(radius: usize) -> f64 {
-        (4.0 / 3.0) * std::f64::consts::PI * (radius.pow(3) as f64)
-    }
-
-    pub(crate) fn triangular_pyramid_volume(base_area: f64, height: usize) -> f64 {
-        (base_area * height as f64) / 3.0
-    }
-
-    pub(crate) fn parallelepiped_volume(side_a: usize, side_b: usize, side_c: usize) -> usize {
-        side_a * side_b * side_c
-    }
-
-    pub(crate) fn cone_volume(base_radius: usize, height: usize) -> f64 {
-        (1.0 / 3.0) * std::f64::consts::PI * base_radius.pow(2) as f64 * height as f64
-    }
+pub enum GeometricalShapes {
+    Square,
+    Circle,
+    Rectangle,
+    Triangle,
 }
 
-pub fn area_fit(
-    (x, y): (usize, usize),
-    kind: areas_volumes::GeometricalShapes,
-    times: usize,
-    (a, b): (usize, usize),
-) -> bool {
-    let rectangle_area = x * y;
-    let shape_area: f64 = match kind {
-        areas_volumes::GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
-        areas_volumes::GeometricalShapes::Circle => areas_volumes::circle_area(a),
-        areas_volumes::GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
-        areas_volumes::GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
-    };
-    shape_area * times as f64 <= rectangle_area as f64
+pub enum GeometricalVolumes {
+    Cube,
+    Sphere,
+    Cone,
+    TriangularPyramid,
+    Parallelepiped,
 }
 
-pub fn volume_fit(
-    (x, y, z): (usize, usize, usize),
-    kind: areas_volumes::GeometricalVolumes,
-    times: usize,
-    (a, b, c): (usize, usize, usize),
-) -> bool {
-    let box_volume = x * y * z;
-    let shape_volume: f64 = match kind {
-        areas_volumes::GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
-        areas_volumes::GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
-        areas_volumes::GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
-        areas_volumes::GeometricalVolumes::TriangularPyramid => {
-            areas_volumes::triangular_pyramid_volume(a as f64, b)
-        }
-        areas_volumes::GeometricalVolumes::Parallelepiped => {
-            areas_volumes::parallelepiped_volume(a, b, c) as f64
-        }
-    };
-    shape_volume * times as f64 <= box_volume as f64
+pub(crate) fn square_area(side: usize) -> usize {
+    side.pow(2)
+}
+
+pub(crate) fn triangle_area(base: usize, height: usize) -> f64 {
+    (base as f64 * height as f64) / 2.0
+}
+
+pub(crate) fn circle_area(radius: usize) -> f64 {
+    std::f64::consts::PI * (radius.pow(2) as f64)
+}
+
+pub(crate) fn rectangle_area(side_a: usize, side_b: usize) -> usize {
+    side_a * side_b
+}
+
+pub(crate) fn cube_volume(side: usize) -> usize {
+    side.pow(3)
+}
+
+pub(crate) fn sphere_volume(radius: usize) -> f64 {
+    (4.0 / 3.0) * std::f64::consts::PI * (radius.pow(3) as f64)
+}
+
+pub(crate) fn triangular_pyramid_volume(base_area: f64, height: usize) -> f64 {
+    (base_area * height as f64) / 3.0
+}
+
+pub(crate) fn parallelepiped_volume(side_a: usize, side_b: usize, side_c: usize) -> usize {
+    side_a * side_b * side_c
+}
+
+pub(crate) fn cone_volume(base_radius: usize, height: usize) -> f64 {
+    (1.0 / 3.0) * std::f64::consts::PI * base_radius.pow(2) as f64 * height as f64
 }
--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,9 +1,5 @@
-pub mod areas_volumes;
-
-use areas_volumes::areas_volumes::{
-    GeometricalShapes, GeometricalVolumes,
-    square_area, circle_area, rectangle_area, triangle_area,
-    cube_volume, sphere_volume, cone_volume, triangular_pyramid_volume, parallelepiped_volume
+use areas_volumes::{
+    self, GeometricalShapes, GeometricalVolumes,
 };
 
 pub fn area_fit(
@@ -12,14 +8,14 @@ pub fn area_fit(
     times: usize,
     (a, b): (usize, usize),
 ) -> bool {
-    let max_size = x * y;
-    let size = match kind {
-        GeometricalShapes::Square => square_area(a) as f64,
-        GeometricalShapes::Circle => circle_area(a),
-        GeometricalShapes::Rectangle => rectangle_area(a, b) as f64,
-        GeometricalShapes::Triangle => triangle_area(a, b),
+    let area_container = x * y;
+    let shape_area = match kind {
+        GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
+        GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
+        GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
+        GeometricalShapes::Circle => areas_volumes::circle_area(a),
     };
-    times as f64 * size <= max_size as f64
+    (shape_area * times as f64) <= area_container as f64
 }
 
 pub fn volume_fit(
@@ -28,49 +24,16 @@ pub fn volume_fit(
     times: usize,
     (a, b, c): (usize, usize, usize),
 ) -> bool {
-    let max_size = x * y * z;
-    let size = match kind {
-        GeometricalVolumes::Cube => cube_volume(a) as f64,
-        GeometricalVolumes::Sphere => sphere_volume(a),
-        GeometricalVolumes::Cone => cone_volume(a, b),
-        GeometricalVolumes::TriangularPyramid => triangular_pyramid_volume(triangle_area(a, b), c),
-        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
+    let volume_container = x * y * z;
+    let shape_volume = match kind {
+        GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
+        GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
+        GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
+        GeometricalVolumes::TriangularPyramid => {
+            let base_area = a as f64; // `a` is base_area here
+            areas_volumes::triangular_pyramid_volume(base_area, b)
+        }
+        GeometricalVolumes::Parallelepiped => areas_volumes::parallelepiped_volume(a, b, c) as f64,
     };
-    times as f64 * size <= max_size as f64
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn no_volumes_shapes() {
-        assert_eq!(true, area_fit((2, 5), GeometricalShapes::Circle, 0, (2, 1)));
-        assert_eq!(true, area_fit((2, 2), GeometricalShapes::Rectangle, 0, (6, 10)));
-        assert_eq!(true, volume_fit((2, 5, 3), GeometricalVolumes::Cone, 0, (1, 1, 1)));
-        assert_eq!(true, volume_fit((3, 5, 7), GeometricalVolumes::Parallelepiped, 0, (2, 6, 3)));
-    }
-
-    #[test]
-    fn equal_size() {
-        assert_eq!(true, area_fit((2, 5), GeometricalShapes::Square, 1, (2, 5)));
-        assert_eq!(true, volume_fit((3, 1, 4), GeometricalVolumes::Cube, 1, (1, 3, 4)));
-    }
-
-    #[test]
-    fn all_shapes() {
-        assert_eq!(false, area_fit((3, 5), GeometricalShapes::Circle, 2, (2, 0)));
-        assert_eq!(true, area_fit((8, 6), GeometricalShapes::Triangle, 5, (5, 2)));
-        assert_eq!(true, area_fit((7, 3), GeometricalShapes::Rectangle, 2, (2, 4)));
-        assert_eq!(true, area_fit((5, 5), GeometricalShapes::Square, 1, (2, 4)));
-    }
-
-    #[test]
-    fn all_volumes() {
-        assert_eq!(true, volume_fit((5, 6, 3), GeometricalVolumes::Cube, 2, (3, 3, 4)));
-        assert_eq!(false, volume_fit((7, 4, 4), GeometricalVolumes::Cone, 1, (8, 2, 0)));
-        assert_eq!(true, volume_fit((2, 5, 3), GeometricalVolumes::Sphere, 1, (1, 1, 1)));
-        assert_eq!(false, volume_fit((2, 5, 3), GeometricalVolumes::Parallelepiped, 31, (1, 1, 1)));
-        assert_eq!(true, volume_fit((7, 5, 3), GeometricalVolumes::TriangularPyramid, 3, (3, 2, 1)));
-    }
+    (shape_volume * times as f64) <= volume_container as f64
 }
