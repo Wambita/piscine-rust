8b5f78c917fa42217c239fc66c05f7de5ab38abc
Wambita <wambitafana@gmail.com>
2025-04-20 15:26:46 +0300

    traitsb

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,31 +1,51 @@
-#[derive(Clone)]
-pub struct StringValue {
-    pub value: String,
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Player {
+    pub name: String,
+    pub strength: f64,
+    pub score: i32,
+    pub money: i32,
+    pub weapons: Vec<String>,
 }
 
-pub trait AppendStr {
-    fn append_str(&mut self, str_to_append: String) -> Self;
-    fn append_number(&mut self, nb_to_append: f64) -> Self;
-    fn remove_punctuation_marks(&mut self) -> Self;
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}\nStrength: {}, Score: {}, Money: {}\nWeapons: {:?}",
+            self.name, self.strength, self.score, self.money, self.weapons
+        )
+    }
+}
+pub struct Fruit {
+    pub weight_in_kg: f64,
 }
 
-impl AppendStr for StringValue {
-    fn append_str(&mut self, str_to_append: String) -> Self {
-        self.value.push_str(&str_to_append);
-        self.clone()
-    }
+pub struct Meat {
+    pub weight_in_kg: f64,
+    pub fat_content: f64,
+}
 
-    fn append_number(&mut self, nb_to_append: f64) -> Self {
-        self.value.push_str(&nb_to_append.to_string());
-        self.clone()
+impl Player {
+    pub fn eat<T: Food>(&mut self, food: T) {
+        self.strength += food.gives();
     }
+}
 
-    fn remove_punctuation_marks(&mut self) -> Self {
-        self.value = self
-            .value
-            .chars()
-            .filter(|c| !matches!(c, '.' | ',' | '?' | '!'))
-            .collect();
-        self.clone()
+pub trait Food {
+    fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    fn gives(&self) -> f64 {
+        self.weight_in_kg * 4.0
+    }
+}
+
+impl Food for Meat {
+    fn gives(&self) -> f64 {
+        ((1.0 - self.fat_content) * self.weight_in_kg * 4.0)
+            + (self.fat_content * self.weight_in_kg * 9.0)
     }
 }
