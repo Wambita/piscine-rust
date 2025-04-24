8ebfe9a437f1bc2a9bb407313670ee59de74eb62
shfana <wambitafana@gmail.com>
2025-04-25 02:20:17 +0300

    feat: adding twice

---
+++ adding_twice/src/lib.rs
@@ -0,0 +1,8 @@
+use lalgebra_scalar::Scalar;
+pub fn add_curry(num: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(move |x: i32| x + num)
+}
+
+pub fn twice<T: Scalar<Item = T> + Clone + std::ops::Add<Output = T> + 'static>(F: Box<dyn Fn(T) -> T>) -> Box<dyn Fn(T) -> T>{
+    Box::new(move |x| F(T::zero()) + F(T::zero()) + x)
+}
