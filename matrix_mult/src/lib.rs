43f0b80f70393e80f5e638898d4278f57c4003bc
shfana <wambitafana@gmail.com>
2025-04-24 19:16:27 +0300

    feat: fix matrix mult

--- matrix_mult/src/lib.rs
+++ matrix_mult/src/lib.rs
@@ -1,13 +1,16 @@
 use std::ops::Mul;
-
-use lalgebra_scalar::Scalar;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-impl<T: Scalar> Matrix<T> {
+impl<T: Copy> Matrix<T> {
     pub fn number_of_cols(&self) -> usize {
-        self.0[0].len()
+        if self.0.is_empty() || self.0[0].is_empty() {
+            0
+        } else {
+            self.0[0].len()
+        }
     }
 
     pub fn number_of_rows(&self) -> usize {
@@ -23,24 +26,70 @@ impl<T: Scalar> Matrix<T> {
     }
 }
 
-impl<T: Scalar> Mul for Matrix<T> {
+impl<T> Mul for Matrix<T>
+where
+    T: Copy + Mul<Output = T> + std::iter::Sum + Default,
+{
     type Output = Option<Matrix<T>>;
 
-    fn mul(self, other: Matrix<T>) -> Option<Matrix<T>> {
-        if self.number_of_cols() != other.number_of_rows() {
+    fn mul(self, rhs: Self) -> Self::Output {
+        let rows_a = self.number_of_rows();
+        let cols_a = self.number_of_cols();
+        let rows_b = rhs.number_of_rows();
+        let cols_b = rhs.number_of_cols();
+
+        if cols_a != rows_b {
             return None;
         }
 
-        let mut result = vec![vec![T::zero(); other.number_of_cols()]; self.number_of_rows()];
+        let mut result = vec![vec![T::default(); cols_b]; rows_a];
 
-        for i in 0..self.number_of_rows() {
-            for j in 0..other.number_of_cols() {
-                for k in 0..self.number_of_cols() {
-                    result[i][j] = result[i][j] + self.0[i][k] * other.0[k][j];
-                }
+        for i in 0..rows_a {
+            for j in 0..cols_b {
+                result[i][j] = (0..cols_a)
+                    .map(|k| self.0[i][k] * rhs.0[k][j])
+                    .sum();
             }
         }
 
         Some(Matrix(result))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matrix_multiplication() {
+        let matrix_a = Matrix(vec![vec![1, 2], vec![3, 4]]);
+        let matrix_b = Matrix(vec![vec![5, 6], vec![7, 8]]);
+        let result = matrix_a * matrix_b;
+
+        assert_eq!(
+            result,
+            Some(Matrix(vec![vec![19, 22], vec![43, 50]]))
+        );
+    }
+
+    #[test]
+    fn test_invalid_multiplication() {
+        let matrix_a = Matrix(vec![vec![1, 2], vec![3, 4]]);
+        let matrix_b = Matrix(vec![vec![5, 6]]);
+
+        let result = matrix_a * matrix_b;
+        assert_eq!(result, None);
+    }
+    #[test]
+    fn test_matrix_row() {
+        let matrix = Matrix(vec![vec![1, 2, 3], vec![4, 5, 6]]);
+        let row = matrix.row(1);
+        assert_eq!(row, vec![4, 5, 6]);
+    }
+    #[test]
+    fn test_matrix_col() {
+        let matrix = Matrix(vec![vec![1, 2, 3], vec![4, 5, 6]]);
+        let col = matrix.col(1);
+        assert_eq!(col, vec![2, 5]);
     }
 }
