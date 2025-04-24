6bf4fd6cd34e699713665f9b16667c769f1d4ee4
shfana <wambitafana@gmail.com>
2025-04-25 00:48:39 +0300

    fix expected var

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,43 +1,20 @@
-fn edit_distance(source: &str, target: &str) -> usize {
-    let len1 = source.chars().count();
-    let len2 = target.chars().count();
-    let mut dp = vec![vec![0; len2 + 1]; len1 + 1];
-    for i in 0..=len1 {
-        for j in 0..=len2 {
-            if i == 0 {
-                dp[i][j] = j;
-            } else if j == 0 {
-                dp[i][j] = i;
-            } else {
-                let cost = if source.chars().nth(i - 1) == target.chars().nth(j - 1) {
-                    0
-                } else {
-                    1
-                };
-                dp[i][j] = (dp[i - 1][j - 1] + cost)
-                    .min(dp[i - 1][j] + 1)
-                    .min(dp[i][j - 1] + 1);
-            }
-        }
-    }
-    dp[len1][len2]
-}
-fn is_snake_case(s: &str) -> bool {
-    s.chars().all(|c| c.is_lowercase() || c == '_')
-}
-pub fn expected_variable(a: &str, b: &str) -> Option<String> {
-    if a.contains(' ') || b.contains(' ') {
+use convert_case::{Case, Casing};
+use edit_distance::edit_distance;
+
+pub fn expected_variable(compare: &str, expected: &str) -> Option<String> {
+    let compare = compare.to_lowercase();
+    let expected = expected.to_lowercase();
+
+    if !compare.is_case(Case::Camel) && !compare.is_case(Case::Snake) {
         return None;
     }
-    let snake_a = a.to_lowercase();
-    let snake_b = b.to_lowercase();
-    if is_snake_case(&snake_a) && is_snake_case(&snake_b) {
-        let distance = edit_distance(&snake_a, &snake_b);
-        let similarity = 1.0 - (distance as f64 / b.len() as f64);
-        let res = (similarity * 100.0).round();
-        if res > 50.0 {
-            return Some(format!("{}%", res));
-        }
+
+    let distance = edit_distance(&compare, &expected);
+
+    let percentage = 100 - (distance * 100 / expected.len()).min(100);
+    if percentage >= 50 {
+        Some(format!("{percentage}%"))
+    } else {
+        None
     }
-    None
 }
