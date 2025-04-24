ef7ec5a940d7fae97b77915b67332163852341a5
shfana <wambitafana@gmail.com>
2025-04-25 02:24:43 +0300

    feat: get products

---
+++ get_products/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    let mut result: Vec<usize> = Vec::new();
+
+    if arr.len() == 1{
+        return result;
+    }
+
+    for cur_ind in 0..arr.len() {
+        let mut product = 1;
+        for ind in 0..arr.len() {
+            if ind != cur_ind{
+                product *= arr[ind];
+            }
+        }
+        result.push(product);
+    }
+
+    return result;
+}
