7ccd047cee70e7dcefef0fa6269565ac81bee6aa
shfana <wambitafana@gmail.com>
2025-04-24 19:41:37 +0300

    feat: fix

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,41 +1,49 @@
-mod areas_volumes;
+pub mod areas_volumes;
 
+pub use areas_volumes::*;
+
+#[inline]
+fn generic_fits(max_size: usize, size: f64, times: usize) -> bool {
+    times as f64 * size <= max_size as f64
+}
+
+#[inline]
 pub fn area_fit(
     (x, y): (usize, usize),
-    kind: areas_volumes::GeometricalShapes,
+    kind: GeometricalShapes,
     times: usize,
     (a, b): (usize, usize),
 ) -> bool {
-    let container_area = (x * y) as f64;
-    let shape_area = match kind {
-        areas_volumes::GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
-        areas_volumes::GeometricalShapes::Circle => areas_volumes::circle_area(a),
-        areas_volumes::GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
-        areas_volumes::GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
-    };
-    let total_area = (times as f64) * shape_area;
-    total_area.ceil() <= container_area
+    generic_fits(
+        x * y,
+        match kind {
+            GeometricalShapes::Square => square_area(a) as _,
+            GeometricalShapes::Circle => circle_area(a),
+            GeometricalShapes::Rectangle => rectangle_area(a, b) as _,
+            GeometricalShapes::Triangle => triangle_area(a, b),
+        },
+        times,
+    )
 }
 
+#[inline]
 pub fn volume_fit(
     (x, y, z): (usize, usize, usize),
-    kind: areas_volumes::GeometricalVolumes,
+    kind: GeometricalVolumes,
     times: usize,
     (a, b, c): (usize, usize, usize),
 ) -> bool {
-    let container_volume = (x * y * z) as f64;
-    let shape_volume = match kind {
-        areas_volumes::GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
-        areas_volumes::GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
-        areas_volumes::GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
-        areas_volumes::GeometricalVolumes::TriangularPyramid => {
-            let base_area = areas_volumes::triangle_area(a, a) as f64;
-            areas_volumes::triangular_pyramid_volume(base_area, b)
-        }
-        areas_volumes::GeometricalVolumes::Parallelepiped => {
-            areas_volumes::parallelepiped_volume(a, b, c) as f64
-        }
-    };
-    let total_volume = (times as f64) * shape_volume;
-    total_volume.ceil() <= container_volume
+    generic_fits(
+        x * y * z,
+        match kind {
+            GeometricalVolumes::Cube => cube_volume(a) as _,
+            GeometricalVolumes::Sphere => sphere_volume(a),
+            GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as _,
+            GeometricalVolumes::TriangularPyramid => {
+                triangular_pyramid_volume(triangle_area(a, b), c)
+            }
+            GeometricalVolumes::Cone => cone_volume(a, b),
+        },
+        times,
+    )
 }
